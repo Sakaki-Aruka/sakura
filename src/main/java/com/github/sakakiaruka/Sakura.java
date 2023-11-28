adf77b1433e8c70420f15fffc29ad780e1e365cb
SakakiAruka <yshida1pc7@gmail.com>
2023-11-28 23:04:56 +0900

    update : refactor

--- src/main/java/com/github/sakakiaruka/Sakura.java
+++ src/main/java/com/github/sakakiaruka/Sakura.java
@@ -7,10 +7,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Sakura extends JavaPlugin {
 
     public static FileConfiguration DEFAULT_CONFIG;
+    public static Sakura instance;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        setInstance();
         getServer().getPluginManager().registerEvents(new Cut(), this);
     }
 
@@ -19,7 +21,10 @@ public final class Sakura extends JavaPlugin {
         // Plugin shutdown logic
     }
 
+//    private void setInstance() {
+//        DEFAULT_CONFIG = this.getConfig();
+//    }
     private void setInstance() {
-        DEFAULT_CONFIG = this.getConfig();
+        instance = this;
     }
 }
--- src/main/java/com/github/sakakiaruka/listeners/Cut.java
+++ src/main/java/com/github/sakakiaruka/listeners/Cut.java
@@ -24,54 +24,40 @@ public class Cut implements Listener {
         Set<Block> blocks = new HashSet<>();
         Set<Block> visited = new HashSet<>();
         List<Block> stack = new ArrayList<>();
-        blocks.add(target);
-        int x = target.getX();
-        int y = target.getY();
-        int z = target.getZ();
+        search(target, blocks, visited, stack);
 
-        for (int dx = -1; dx <= 1; dx++) {
-            for (int dz = -1; dz <= 1; dz++) {
-                Block block = target.getWorld().getBlockAt(x+dx, y, z+dz);
-                if (isLog(block)) stack.add(block);
-            }
-        }
-
-        int limit = 500;
-        int buffer = 0;
-        for (int i = 0; i < limit; i++) {
-            Block block = target.getWorld().getBlockAt(x, y+i, z);
-            search(block, blocks, visited, stack);
-            if (Math.abs(blocks.size() - buffer) == 0) break;
-            buffer = blocks.size();
-        }
-
-        blocks.forEach(block -> block.getDrops().forEach(item -> cutter.getWorld().dropItem(cutter.getLocation(), item)));
+        blocks.forEach(block -> {
+            block.getDrops().forEach(item -> cutter.getWorld().dropItem(cutter.getLocation(), item));
+            block.setType(Material.AIR);
+        });
     }
 
     private boolean isLog(Block block) {
-        Material material = block.getType();
-        String name = material.name();
-        if (name.matches("^([A-Z_]+)_LOG$")) return true;
-        if (name.matches("^([A-Z_]+)_STEM$")) return true;
-        return false;
+        return block.getType().name().matches("^([A-Z_]+)_(LOG|STEM)$");
+    }
+
+    private boolean isLeaf(Block block) {
+        return block.getType().name().matches("^([A-Z_]+)_(LEAVES|WART_BLOCK)$");
     }
 
     private void search(Block start, Set<Block> blocks, Set<Block> visited, List<Block> stack) {
         int x = start.getX();
-        int y = start.getY() + 1;
+        int y = start.getY();
         int z = start.getZ();
         World world = start.getWorld();
 
         stack.remove(start);
-        if (isLog(start)) {
+        if (isLog(start) || isLeaf(start)) {
             blocks.add(start);
             visited.add(start);
         }
         for (int dx = -1; dx <= 1; dx++) {
-            for (int dz = -1; dz <= 1; dz++) {
-                Block block = world.getBlockAt(x+dx, y, z+dz);
-                if (isLog(block) && !stack.contains(block) && !visited.contains(block)) stack.add(0, block);
-                if (!stack.isEmpty()) search(stack.get(0), blocks, visited, stack);
+            for (int dy = -1; dy <= 1; dy++) {
+                for (int dz = -1; dz <= 1; dz++) {
+                    Block block = world.getBlockAt(x+dx, y+dy, z+dz);
+                    if ((isLog(block) || isLeaf(block)) && !stack.contains(block) && !visited.contains(block)) stack.add(0, block);
+                    if (!stack.isEmpty()) search(stack.get(0), blocks, visited, stack);
+                }
             }
         }
     }
